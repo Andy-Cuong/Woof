ca3377353636eed209c43cd926bdbec8560b606b
Andy Cuong <nguyenduccuong11102003@gmail.com>
2024-11-18 15:43:52 +0200

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,7 +68,7 @@ android {
 
 dependencies {
 
-    implementation(platform("androidx.compose:compose-bom:2024.10.01"))
+    implementation(platform("androidx.compose:compose-bom:2024.11.00"))
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("androidx.compose.material3:material3")
